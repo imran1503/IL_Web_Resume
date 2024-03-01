59340a66e0056e33d70034473d5bfbe702ccf691
Imran Latif <imran.latif1503@gmail.com>
2024-03-01 11:04:27 -0500

    Started working on navbar.

---
+++ react_resume/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar";
+import React from 'react';
+import './App.css';
+export default function App(){
+    return(
+        <div className="app">
+           <Navbar />
+        </div>
+    )
+  
+}
+
--- react_resume/src/Components/Navbar.js
+++ react_resume/src/Components/Navbar.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react'
-
+import {Link} from 'react-router-dom'
 function Navbar() {
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    
+                    <Link to="/" className='navbar-logo'> Imran Latif<i className='fab fa-typo3'></i> </Link>
                 </div>
             </nav>
         </>
     )
 }
 
-export default Navbar
+export default Navbar;
