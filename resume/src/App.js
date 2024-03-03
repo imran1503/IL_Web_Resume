0bc6adbffb7bf4b881543e300b96aa876c064da0
Imran Latif <imran.latif1503@gmail.com>
2024-03-03 14:24:28 -0500

    Cleaned up project files

--- resume/src/App.js
+++ resume/src/App.js
@@ -3,15 +3,14 @@ import Navbar from "./Components/Navbar";
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 function App(){
     return(
-
-      <Navbar/>
-
-
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
     )
-  
 }
-
 export default App;
--- resume/src/Components/Navbar.js
+++ resume/src/Components/Navbar.js
@@ -1,39 +1,39 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 function Navbar() {
-    const [click,setClick] = useState(false);
-    const HandleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+    // const [click,setClick] = useState(false);
+    // const HandleClick = () => setClick(!click);
+    // const closeMobileMenu = () => setClick(false);
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
                 <a to='/' className='navbar-container'>
                     Imran Latif <i className='fab fa-typo3'/>
                 </a>
-                <div className='menu-icon' onClick={HandleClick} >
+                <div className='menu-icon' /* onClick={HandleClick} */ >
                     <i className="" />
                 </div>
-                <ul className=""> 
+                <ul className="">
                     <li className='nav-item'>
-                        <a to='/Home' className="nav-links" onClick={closeMobileMenu}>
+                        <a to='/Home' className="nav-links" /*onClick={closeMobileMenu}*/>
                             Home
                         </a>
                     </li>
                     <li className='nav-item'>
-                        <a to='/aboutMe' className="nav-links" onClick={closeMobileMenu} >
+                        <a to='/aboutMe' className="nav-links" /*onClick={closeMobileMenu}*/ >
                             About me
                         </a>
                     </li>
                     <li className='nav-item'>
-                        <a to='/experience' className="nav-links" onClick={closeMobileMenu} >
+                        <a to='/experience' className="nav-links" /*onClick={closeMobileMenu} */>
                             Experience
                         </a>
                     </li>
                 </ul>
             </div>
-        </nav> 
-       
-        
+        </nav>
+
+
     );
 }
 
