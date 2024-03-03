398081d4cecf3afe3636b0812cc78aa637af03fd
Imran Latif <imran.latif1503@gmail.com>
2024-03-03 13:24:35 -0500

    Base Navbar operational

---
+++ resume/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from "./Components/Navbar";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App(){
+    return(
+
+      <Navbar/>
+
+
+    )
+  
+}
+
+export default App;
