ddfddc3326369dbd6a60c9148e221402b0029249
Imran Latif <imran.latif1503@gmail.com>
2024-03-18 00:26:52 -0400

    Iterated on Timeline.js

--- resume/src/Components/Timeline.js
+++ resume/src/Components/Timeline.js
@@ -9,7 +9,9 @@ function Timeline(){
 
             <div className='Timeline'>
                 <div className='container left-container'>
+                    {/* <img src={process.env.PUBLIC_URL + '../../public/images/MECLogo.png'} alt-text='mec' alt='mec' /> */}
                     <div className='text-box'>
+
                         <h2>Company 1 </h2>
                         <small>Date range Here</small>
                         <p>Description here</p>
@@ -34,6 +36,67 @@ function Timeline(){
                         </Button>
 
                     </div>
+
+                </div>
+                <div className='container left-container'>
+                    {/* <img src={process.env.PUBLIC_URL + '../../public/images/MECLogo.png'} alt-text='mec' alt='mec' /> */}
+                    <div className='text-box'>
+
+                        <h2>Company 1 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+                    </div>
+                </div>
+                <div className='container right-container'>
+                    <div className='text-box'>
+                        <h2>Company 2 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+
+                    </div>
+
+                </div>
+                <div className='container left-container'>
+                    {/* <img src={process.env.PUBLIC_URL + '../../public/images/MECLogo.png'} alt-text='mec' alt='mec' /> */}
+                    <div className='text-box'>
+
+                        <h2>Company 1 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+                    </div>
+                </div>
+                <div className='container right-container'>
+                    <div className='text-box'>
+                        <h2>Company 2 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+
+                    </div>
+
                 </div>
             </div>
         </div>
