8c1ba9ead2e68f1d1b6bd6a277a40ee5f463b97c
Imran Latif <imran.latif1503@gmail.com>
2024-03-17 22:53:31 -0400

    Iterated on Timeline

--- resume/src/Components/Timeline.js
+++ resume/src/Components/Timeline.js
@@ -5,10 +5,11 @@ import { Button } from './Button';
 
 function Timeline(){
     return(
-        <body className='body'>
+        <div className='body'>
 
             <div className='Timeline'>
-                <div className='container'>
+                <div className='container left-container'>
+                    <img src={process.env.PUBLIC_URL + '../../public/images/MirrorsEdgeLogo.png'} />;
                     <div className='text-box'>
                         <h2>Company 1 </h2>
                         <small>Date range Here</small>
@@ -21,8 +22,50 @@ function Timeline(){
                         </Button>
                     </div>
                 </div>
+                <div className='container right-container'>
+                    <i class="fa-brands fa-github fa-lg"></i>
+                    <div className='text-box'>
+                        <h2>Company 2 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+                    </div>
+                </div>
+                <div className='container left-container'>
+                    <i class="fa-brands fa-github fa-lg"></i>
+                    <div className='text-box'>
+                        <h2>Company 3 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+                    </div>
+                </div>
+                <div className='container right-container'>
+                    <i class="fa-brands fa-github fa-lg"></i>
+                    <div className='text-box'>
+                        <h2>Company 4 </h2>
+                        <small>Date range Here</small>
+                        <p>Description here</p>
+                        <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn-small'} MyLink={
+                            "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                            }
+                        >
+                            Click for more info!
+                        </Button>
+                    </div>
+                </div>
             </div>
-        </body>
+        </div>
     );
 }
 
