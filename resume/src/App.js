5b428bd5d8ff031fe244ea56afb2d66b22480e98
Imran Latif <imran.latif1503@gmail.com>
2024-04-22 18:42:46 -0400

    Reverting HashBrowser

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,29 +1,20 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/pages/home.js';
 import Experience from './Components/pages/Experience.js';
-import contact from './Components/pages/Contact.js';// Test
+
 function App(){
     return(
       <Router>
 
         <Navbar/>
         <Routes>
-          <Route path='/contact' Component={contact} />
           <Route path='/experience' Component={Experience} />
           <Route path='/' Component={Home} />
           <Route path='/IL_Web_Resume' Component={Home} />
           <Route path="*" Component={Home}/>
-
-
-
-
-
-
-
         </Routes>
       </Router>
     )
