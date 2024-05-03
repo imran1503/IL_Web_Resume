0b0064532fa9aae7c13d1aef516e46b19ea991e0
Imran Latif <imran.latif1503@gmail.com>
2024-05-03 18:30:02 -0400

    Updating Hero Section Layout on Mobile

--- resume/src/Components/HeroSection.js
+++ resume/src/Components/HeroSection.js
@@ -12,9 +12,7 @@ function HeroSection(){
             <div className="banner-text">
                 <h1 className='headline'>IMRAN LATIF</h1>
                 <h3 className='subheadline'>
-                    <strong>New software graduate unleashing the potential of tomorrow's tech leaders.<br/>
-                            Where passion meets proficiency in software engineering.
-                    </strong>
+                    <strong>Where passion meets proficiency in software engineering.</strong>
                 </h3>
                 <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn--medium'} MyLink={
                     "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
