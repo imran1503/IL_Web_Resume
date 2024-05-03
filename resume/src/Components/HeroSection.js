e0c5600cf13a3f9b85494b53b6b6c79358900492
Imran Latif <imran.latif1503@gmail.com>
2024-05-03 18:39:35 -0400

    Updated Resume Download Link

--- resume/src/Components/HeroSection.js
+++ resume/src/Components/HeroSection.js
@@ -15,7 +15,7 @@ function HeroSection(){
                     <strong>Where passion meets proficiency in software engineering.</strong>
                 </h3>
                 <Button className='hero-btns' buttonStyle={"btn--outline"} buttonSize={'btn--medium'} MyLink={
-                    "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=drive_link"
+                    "https://drive.google.com/file/d/1IvEfbMABQE1N9YbAoIp5nBFwhMw95CRG/view?usp=sharing"
                     }
                 >
                     Download my Resume!
